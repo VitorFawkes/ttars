2b3e5a2c9449e44468b0915974c5c7c01eea8064
Vitor Gambetti <vitorgambetti@gmail.com>
2026-03-30 20:55:26 -0300

    deleted and modified

--- validation_queries_strategic.sql
+++
@@ -1,6 +0,0 @@
--- No new schema changes for this phase, but we can verify the existing structure again.
--- Verify participantes_externos column exists
-SELECT column_name, data_type 
-FROM information_schema.columns 
-WHERE table_name = 'tarefas' 
-AND column_name = 'participantes_externos';
